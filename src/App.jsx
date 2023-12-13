2748d69b353196fc4ca182c79ddad4941b0c2246
Elea641 <eleavoliotis64@gmail.com>
2023-12-13 10:56:20 +0100

    add button connect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import "./App.css";
+import { useAppDemo } from "./context/AppContext";
 
 function App() {
+  const { login, logout, user } = useAppDemo();
+
   return (
     <>
       <Outlet />
+      <h1>{user.admin ? "connecté" : "déconnecté"}</h1>
+      <button onClick={login}>Login</button>
+      <Link to="/admin/demo">AdminDemo</Link>
+      <button onClick={logout}>Logout</button>
+      <Link to="/demo">Demo</Link>
     </>
   );
 }
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -4,12 +4,20 @@ const appContext = createContext();
 
 const AppContextProvider = ({ children }) => {
   const [isAdmin, setIsAdmin] = useState(false);
-  const [user, setUser] = useState({ admin: true });
+  const [user, setUser] = useState({ admin: false });
+
+  const login = () => {
+    setUser({ admin: true });
+  };
+
+  const logout = () => {
+    setUser({ admin: false });
+  };
 
   //exemple méthodes pour communiquer avec une api
 
   return (
-    <appContext.Provider value={{ isAdmin, user }}>
+    <appContext.Provider value={{ isAdmin, user, login, logout }}>
       {children}
     </appContext.Provider>
   );
